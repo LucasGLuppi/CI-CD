86add10b130cb2883b0b02b49f20c696f95b2d71
Lucas Luppi <lucasgluppi@gmail.com>
2025-05-09 11:56:22 -0300

    Agregación de funciones en test_app.py

--- test_app.py
+++ test_app.py
@@ -5,3 +5,17 @@ def suma(a, b):
 def test_suma():
     assert suma(2, 3) == 5
     assert suma(-1, 1) == 0
+
+def resta(a, b):
+    return a - b
+
+def test_resta():
+    assert resta(2, 5) == -3
+    assert resta(4, 1) == 3
+    
+def multiplicacion(a, b):
+    return a * b
+
+def test_multiplicacion():
+    assert multiplicacion(2, 5) == 10
+    assert multiplicacion(3, 11) == 33
